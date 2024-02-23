caf47ae1e2a1782067134d6e994ebb3ac68d11e2
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-23 19:02:27 +0100

    add: aggiunto css nei corm

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -3,52 +3,54 @@
 <title>Home</title>
 @endsection
 @section('content')
-<h1>
-    tipi di progetto
-</h1>
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-<!-- form per crearer un nuovo progetto -->
-<form action="{{route('pages.store')}}" method="POST">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <!-- form per crearer un nuovo progetto -->
+            <form action="{{route('pages.store')}}" method="POST">
 
-    @csrf
-    @method('POST')
-    <!-- valore che da il nuovo progetto -->
-    <label for="nome_progetto">metti nome progetto</label>
-    <input type="text" id="nome_progetto" name="nome_progetto">
-    <br>
-    <!-- valore che da il giorno che hai iniziato il  progetto -->
-    <label for="inizio_progetto">data inizio progetto</label>
-    <input type="date" name="inizio_progetto" id="inizio_progetto">
-    <br>
-    <!-- valore che da la descrizione del progetto -->
-    <label for="descrizione">metti descrizione del progetto</label>
-    <input type="text" id="descrizione" name="descrizione">
-    <br>
-    <!-- valore che da la possibilità di selezioneare il progetto nuovo progetto -->
-    <h2>selezione il tipo di progetto</h2>
-    <select name="nome" id="nome">
-        @foreach($types as $type)
-        <option value="{{$type->id}}">{{$type->nome}}</option>
-        @endforeach
-    </select>
-    <h2>selezione il tipo di tecnologia </h2>
-    <!-- valore che da la possibilitàdi selezionare la tecnologia  -->
-    @foreach ($technologies as $technology)
-    <input type="checkbox" name="technology_id[]" id="technology_id[]" value="{{ $technology -> id }}">
-    <label for="technology_id[]">
-        {{ $technology -> nome_tecnologia }}
-    </label>
-    <br>
-    @endforeach
-    <br>
-    <input type="submit" value="crea progetto">
-</form>
+                @csrf
+                @method('POST')
+                <!-- valore che da il nuovo progetto -->
+                <label for="nome_progetto">metti nome progetto</label>
+                <input class="form-control my-3" type="text" placeholder="Default input" aria-label="default input example" name="nome_progetto">
+                <!-- valore che da il giorno che hai iniziato il  progetto -->
+                <label for="inizio_progetto my-3">data inizio progetto</label>
+                <input type="date" name="inizio_progetto" id="inizio_progetto">
+
+                <!-- valore che da la descrizione del progetto -->
+                <label for="descrizione">metti descrizione del progetto</label>
+                <textarea class="form-control my-3" id="exampleFormControlTextarea1" rows="3" name="descrizione"></textarea>
+
+                <!-- valore che da la possibilità di selezioneare il progetto nuovo progetto -->
+                <h2>selezione il tipo di progetto</h2>
+                <select class="form-select my-3" aria-label="Default select example" name="nome" id="nome">
+                    @foreach($types as $type)
+                    <option value="{{$type->id}}">{{$type->nome}}</option>
+                    @endforeach
+                </select>
+                <h2>selezione il tipo di tecnologia </h2>
+                <!-- valore che da la possibilitàdi selezionare la tecnologia  -->
+                @foreach ($technologies as $technology)
+                <div class="form-check">
+                    <input class="form-check-input float-none" type="checkbox" value="{{ $technology -> id }}" id="technology_id[]" name="technology_id[]">
+                    <label class="form-check-label">
+                        {{$technology->nome_tecnologia}}
+                    </label>
+                </div>
+                @endforeach
+                <input type="submit" value="crea progetto" class="my-3">
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -4,51 +4,54 @@
 @endsection
 @section('content')
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <!-- form per upgrade method in controller  -->
+            <form action="{{route('pages.upgrade', $project->id)}}" method="POST">
 
-<!-- form per upgrade method in controller  -->
-<form action="{{route('pages.upgrade', $project->id)}}" method="POST">
+                @csrf
+                @method('PUT')
+                <!-- valore che da il nuovo progetto -->
+                <label for="nome_progetto">metti nome progetto</label>
+                <input class="form-control my-3" type="text" placeholder="Default input" aria-label="default input example" name="nome_progetto" value="{{$project->nome_progetto}}">
+                <!-- valore che da il giorno che hai iniziato il  progetto -->
+                <label for="inizio_progetto my-3">data inizio progetto</label>
+                <input type="date" name="inizio_progetto" id="inizio_progetto" value="{{$project->inizio_progetto}}">
 
-    @csrf
-    @method('PUT')
-<!-- nome progetto -->
-    <label for="nome_progetto">metti nome progetto</label>
-    <input type="text" id="nome_progetto" name="nome_progetto" value="{{$project->nome_progetto}}">
-    <br>
-    <!-- data progetto -->
-    <label for="inizio_progetto">data inizio progetto</label>
-    <input type="date" name="inizio_progetto" id="inizio_progetto" value="{{$project->inizio_progetto}}">
-    <br>
-    <!-- descrizione progetto -->
-    <label for="descrizione">metti descrizione del progetto</label>
-    <input type="text" id="descrizione" name="descrizione" value="{{$project->descrizione}}">
-    <br>
-    <h2>selezione il tipo di progetto</h2>
-    <!-- stampare il type di progetto a cui appartiene il progetto selezionato, con condizione che fa vedere quello selezionato -->
-    <select name="nome" id="nome">
-        @foreach($types as $type)
-        <option value="{{$type->id}}" @if($project -> type -> id == $type -> id)
-            selected
-            @endif
-            >{{$type->nome}}</option>
-        @endforeach
-    </select>
-    <!-- stampare i tipi di technology  -->
-    <h2>selezione il tipo di tecnologia </h2>
-    @foreach ($technologies as $technology)
-    <input type="checkbox" name="technology_id[]" id="technology_id[]" value="{{ $technology -> id }}" 
-    @foreach($project -> technologies as $technology_project)
-       @if ($technology_project -> id == $technology -> id)
-          checked
-       @endif
-    @endforeach
+                <!-- valore che da la descrizione del progetto -->
+                <label for="descrizione">metti descrizione del progetto</label>
+                <input class="form-control my-3" aria-label="default input example" placeholder="Default input" type="text" id="descrizione" name="descrizione" value="{{$project->descrizione}}">
 
-    >
-    <label for="technology_id[]">
-        {{ $technology -> nome_tecnologia }}
-    </label>
-    <br>
-    @endforeach
-    <br>
-    <input type="submit" value="aggiorna progetto">
+                <h2>selezione il tipo di progetto</h2>
+                <!-- stampare il type di progetto a cui appartiene il progetto selezionato, con condizione che fa vedere quello selezionato -->
+                <select class="form-select my-3" aria-label="Default select example" name="nome" id="nome">
+                    @foreach($types as $type)
+                    <option value="{{$type->id}}" @if($project -> type -> id == $type -> id)
+                        selected
+                        @endif
+                        >{{$type->nome}}</option>
+                    @endforeach
+                </select>
+                <!-- stampare i tipi di technology  -->
+                <h2>selezione il tipo di tecnologia </h2>
+                @foreach ($technologies as $technology)
+                <div class="form-check">
+                    <input class="form-check-input float-none" type="checkbox" name="technology_id[]" id="technology_id[]" value="{{ $technology -> id }}" @foreach($project -> technologies as $technology_project)
+                    @if ($technology_project -> id == $technology -> id)
+                    checked
+                    @endif
+                    @endforeach
 
-    @endsection
+                    >
+                    <label for="technology_id[]" class="form-check-label">
+                        {{ $technology -> nome_tecnologia }}
+                    </label>
+                </div>
+                @endforeach
+                <br>
+                <input type="submit" value="aggiorna progetto">
+        </div>
+    </div>
+</div>
+@endsection
