b35f42459a56301b36dbcd5b0eafca46671bca46
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-23 18:22:19 +0100

    add: aggiunto commenti nel front-end

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -6,20 +6,24 @@
 <h1>
     tipi di progetto
 </h1>
+<!-- form per crearer un nuovo progetto -->
 <form action="{{route('pages.store')}}" method="POST">
 
     @csrf
     @method('POST')
-
+    <!-- valore che da il nuovo progetto -->
     <label for="nome_progetto">metti nome progetto</label>
     <input type="text" id="nome_progetto" name="nome_progetto">
     <br>
+    <!-- valore che da il giorno che hai iniziato il  progetto -->
     <label for="inizio_progetto">data inizio progetto</label>
     <input type="date" name="inizio_progetto" id="inizio_progetto">
     <br>
+    <!-- valore che da la descrizione del progetto -->
     <label for="descrizione">metti descrizione del progetto</label>
     <input type="text" id="descrizione" name="descrizione">
     <br>
+    <!-- valore che da la possibilità di selezioneare il progetto nuovo progetto -->
     <h2>selezione il tipo di progetto</h2>
     <select name="nome" id="nome">
         @foreach($types as $type)
@@ -27,6 +31,7 @@
         @endforeach
     </select>
     <h2>selezione il tipo di tecnologia </h2>
+    <!-- valore che da la possibilitàdi selezionare la tecnologia  -->
     @foreach ($technologies as $technology)
     <input type="checkbox" name="technology_id[]" id="technology_id[]" value="{{ $technology -> id }}">
     <label for="technology_id[]">
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -5,22 +5,25 @@
 @section('content')
 
 
-
+<!-- form per upgrade method in controller  -->
 <form action="{{route('pages.upgrade', $project->id)}}" method="POST">
 
     @csrf
     @method('PUT')
-
+<!-- nome progetto -->
     <label for="nome_progetto">metti nome progetto</label>
     <input type="text" id="nome_progetto" name="nome_progetto" value="{{$project->nome_progetto}}">
     <br>
+    <!-- data progetto -->
     <label for="inizio_progetto">data inizio progetto</label>
     <input type="date" name="inizio_progetto" id="inizio_progetto" value="{{$project->inizio_progetto}}">
     <br>
+    <!-- descrizione progetto -->
     <label for="descrizione">metti descrizione del progetto</label>
     <input type="text" id="descrizione" name="descrizione" value="{{$project->descrizione}}">
     <br>
     <h2>selezione il tipo di progetto</h2>
+    <!-- stampare il type di progetto a cui appartiene il progetto selezionato, con condizione che fa vedere quello selezionato -->
     <select name="nome" id="nome">
         @foreach($types as $type)
         <option value="{{$type->id}}" @if($project -> type -> id == $type -> id)
@@ -29,6 +32,7 @@
             >{{$type->nome}}</option>
         @endforeach
     </select>
+    <!-- stampare i tipi di technology  -->
     <h2>selezione il tipo di tecnologia </h2>
     @foreach ($technologies as $technology)
     <input type="checkbox" name="technology_id[]" id="technology_id[]" value="{{ $technology -> id }}" 
